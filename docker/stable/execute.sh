6ccf18116a4c42984d9d0276064d371684b92d82
Matt Martz <matt@sivel.net>
2020-04-20 13:07:39 -0500

    Py3

--- docker/stable/execute.sh
+++ docker/stable/execute.sh
@@ -7,4 +7,4 @@ export ANSIBLE_RETRY_FILES_ENABLED=0
 echo $VARIABLES | base64 -d > /variables.yml
 echo $TEMPLATE | base64 -d > /template.j2
 
-timeout -s KILL 5 ansible-playbook -i hosts -e @variables.yml playbook.yml
+timeout -s KILL 5 ansible-playbook -e @variables.yml playbook.yml
